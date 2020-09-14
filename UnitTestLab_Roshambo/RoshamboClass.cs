3d518f863733c493254e790be5f8a46a86d2b71c
Sandy Jurek <sandyjurek@quickenloans.com>
2020-09-14 17:21:18 -0400

    initial code commit

---
+++ UnitTestLab_Roshambo/RoshamboClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoshamboLab
+{
+    public class RoshamboClass
+    {
+        public string Winner(string firstPlayer, string secondPlayer)
+        {
+            string result;
+
+            if ((firstPlayer == "Rock" && secondPlayer == "Scissors") || secondPlayer == "Rock" && firstPlayer == "Scissors")
+            {
+                result = "Rock beat Scissors";
+            }
+            else if ((firstPlayer == "Paper" && secondPlayer == "Rock") || secondPlayer == "Rock" && firstPlayer == "Paper")
+            {
+                result = "Paper beat Rock";
+            }
+            else if ((firstPlayer == "Scissors" && secondPlayer == "Paper") || secondPlayer == "Paper" && firstPlayer == "Scissors")
+            {
+                result = "Scissors beat Paper";
+            }
+
+            else
+            {
+                result = "Tie!";
+            }
+
+            return result;
+        }
+    }
+}
