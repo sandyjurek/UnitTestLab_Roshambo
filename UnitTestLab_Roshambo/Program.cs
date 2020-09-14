2486146937011075430b737a97185d616cb99a38
Sandy Jurek <sandyjurek@quickenloans.com>
2020-09-14 17:15:47 -0400

    Add project files.

---
+++ UnitTestLab_Roshambo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UnitTestLab_Roshambo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UnitTestLab_RoshamboTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitTestLab_RoshamboTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
